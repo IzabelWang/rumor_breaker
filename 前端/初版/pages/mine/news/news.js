567228610e908676b5fca9f64ce48589b2e6b3ac
Izabel Wang <37297881+IzabelWang@users.noreply.github.com>
2020-01-22 14:47:26 +0800

    update

--- 前端/初版/pages/mine/news/news.js
+++ 前端/初版/pages/mine/news/news.js
@@ -1,66 +1,19 @@
 // pages/mine/news/news.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    news: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  onLoad: function () {
+   var that = this;
+   wx.request({
+    url: 'https://www.easy-mock.com/mock/5a23a9a2ff38a436c591b6fa/getArticInfo',
+    success: function(res) {
+      // console.log(res.data.data);
+      // console.log(res.data.data.mine);
+      that.setData({
+        news: res.data.data.news,
+      })
+    }
+   })
+  }, 
 })
